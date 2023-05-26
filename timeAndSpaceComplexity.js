c0cc4b57e9c6549f1ed48895b234e7c5d4f1616c
Tidalu <jsjsdq65@gmail.com>
2023-05-26 14:23:54 +0200

    update

---
+++ timeAndSpaceComplexity.js
@@ -0,0 +1,36 @@
+
+// what is time complexity?
+
+    // in every interview after writing your code you will be asked what is the time complexity of the program and what is the space complexity of it
+
+    //  Time complexity !== time taken
+
+
+    // old widnows              // new macbook
+
+
+    //  okay here, if we have 10{input} macbook can take 1 sec and windows can take 2 sec
+    // input 20 - macbook 2 sek windows 4 sec
+    // here time complexity is the rat between the increasing time takens. 
+    
+    // TC -> BigO notation
+    // every types of code takes time in terms of big O notation
+    // O() it is expressed like this
+
+    // ex  
+
+    for(var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    // first step is initializing
+    // second step is comparising
+    // third step is consoling
+    // fourth is increment
+    // again comparing fifth
+    // again consoling six
+    // seven increment 
+
+
+    // all steps  will be written inside of Big O()
+    // time complexity is computed in worst case scenario 8:43
