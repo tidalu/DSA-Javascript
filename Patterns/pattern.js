7f8afe424803eefb9632a98e117ca05354e6afe2
Tidalu <jsjsdq65@gmail.com>
2023-06-12 01:42:02 +0200

    solved some pattern

--- Patterns/pattern.js
+++ Patterns/pattern.js
@@ -60,3 +60,58 @@ for(var i = 5; i > 0; i--){
     }
     console.log(row);
 }
+
+for(var i = 0; i < 5; i++){
+    for(let j = 4; j > i; j--){
+        process.stdout.write(" ");
+    }
+    for(let k= 0; k <= i*2; k++){
+        process.stdout.write("*");
+    }
+
+    console.log('\n');
+}
+
+
+process.stdout.write("this si another way of writing");
+process.stdout.write("  nega shunaqa"); 
+console.log('t')// the difference is the lne break
+
+
+for(var i = 0; i < 5; i++){
+    
+    for(var j = 0; j < i; j++){
+        process.stdout.write(" ")
+    }
+    for(var k = 0; k < 9-(i*2); k++){
+        process.stdout.write("*");
+    }
+    console.log('\n');
+}
+
+
+for( var i = 0; i < 10; i++){
+    if(i > 5){
+        for(var j = 0; j < i; j++){
+            process.stdout.write(" ")
+        }
+        for(var k = 0; k <= 9-(i*2); k++){
+            process.stdout.write("*");
+        }
+        for(var j = 0; j < i; j++){
+            process.stdout.write(" ")
+        }
+    } else {
+        for(let l = 4; l > i; l--){
+            process.stdout.write(" ");
+        }
+        for(let m= 0; m <= i*2; m++){
+            process.stdout.write("*");
+        }
+        for(let l = 4; l > i; l--){
+            process.stdout.write(" ");
+        }
+    }
+
+    console.log('\n');
+}
