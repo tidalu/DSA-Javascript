100414e7ed0c18046b01bc2d7e3113f9d27b1e28
Tidalu <jsjsdq65@gmail.com>
2023-06-18 23:54:27 +0200

    done reverse number

--- reverseNumber.js
+++ reverseNumber.js
@@ -1,16 +1,7 @@
+
 var reverse = function(x) {
-    var newA = [];
-    Array.from(x).forEach(x => {
-        x > 0 ? newA.push(x) : newA.push(x*(-1));
-    })
-    for( el of newA){
-        var tric ;
-        tric = el;
-        el = newA.at(-1);
-        newA.at(-1) = tric ;
-    }
-    return newA.join('');
+    var arr = Array.from(String(x), x => parseInt(x));
+    arr.filter( x => !isNaN(x));
+    return parseInt(arr.reverse().join(''));
 };
-console.log(reverse(123))
-console.log(reverse(-321))
-console.log(reverse(210))
+
