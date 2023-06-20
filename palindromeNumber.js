85805b8f87f8dcc8ed1ec66f707a44f447b95de6
Tidalu <jsjsdq65@gmail.com>
2023-06-20 23:02:55 +0200

    dsa problems are solved

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -1,9 +1,11 @@
-var isPalindrome = function(x) {
-    // smth wrong with me for now 
-};
+
+    function isPalindrome(stringI) {
+        return String(stringI) == String(stringI.split('').reverse().join('')) ? true : false;
+        // console.log(String(stringI))
+    }
 
 
 
-console.log(isPalindrome(121));
-console.log(isPalindrome(-121));
-console.log(isPalindrome(21));
+console.log(isPalindrome("aabaa"));
+// console.log(isPalindrome(-121));
+// console.log(isPalindrome(21));
