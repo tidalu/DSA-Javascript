b51c4521bd00d9bb1acefe595b43d67a4a6547f5
Tidalu <jsjsdq65@gmail.com>
2023-06-23 11:59:33 +0200

    second solution matrix.sum.js

--- matrixSum.js
+++ matrixSum.js
@@ -13,7 +13,37 @@ function solution(matrix) {
     return sum;
 }
 
+function solution(matrix) {
+    let sum = 0;
+    let rowsWithZero = [];
 
-console.log(solution([[0, 1, 2, 3], [1,2, 0, 4], [5, 4, 3, 2]]))
+    for (let i = 0; i < matrix.length; i++) {
+        let rowSum = 0;
+        let hasZero = false;
+
+        for (let j = 0; j < matrix[i].length; j++) {
+            rowSum += matrix[i][j];
+            if (matrix[i][j] === 0) {
+                hasZero = true;
+                break;
+            }
+        }
+
+        if (hasZero) {
+            rowsWithZero.push(i);
+        }
+
+        sum += rowSum;
+    }
+
+    rowsWithZero.forEach(row => {
+        matrix[row].fill(0);
+    });
+
+    return sum;
+}
+
+
+console.log(solution([[0, 1, 2, 3], [1, 2, 0, 4], [5, 4, 3, 2]]))
 
 // unfinished yet
