e650abda0e743caf75a6468384e8b015336f4f05
Tidalu <jsjsdq65@gmail.com>
2023-06-23 11:55:11 +0200

    update matrix

--- matrixSum.js
+++ matrixSum.js
@@ -1,15 +1,19 @@
+
 function solution(matrix) {
     let sum = 0;
-    for (let i = 1; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
-            if (matrix[i - 1][j] !== 0) {
+            if (matrix[i][j] == 0) {
+                matrix.forEach(row => row[0] = 0);
+            } else {
                 sum += matrix[i][j];
-                console.log(sum);
             }
         }
     }
     return sum;
 }
-console.log(solution([[0, 1, 2, 3], [1,2, 3, 4], [5, 4, 3, 2]]))
+
+
+console.log(solution([[0, 1, 2, 3], [1,2, 0, 4], [5, 4, 3, 2]]))
 
 // unfinished yet
