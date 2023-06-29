ce44f33987a218fe307f5dc3a8f4d41f7886b53e
Tidalu <jsjsdq65@gmail.com>
2023-06-29 13:29:24 +0200

    done some easy level leetcode questions

---
+++ _Logic_Building_/findDiff.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    return new Set(s)
+};
+
+console.log(findTheDifference("aa", "aaa"))
