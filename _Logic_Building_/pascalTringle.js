76c4b9a013d16077c6ea4fdd864d62da4724585f
Tidalu <jsjsdq65@gmail.com>
2023-06-30 16:43:31 +0200

    incomplete: wrote only for one cond, finish later

---
+++ _Logic_Building_/pascalTringle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    var triangle = []
+    if (numRows == 1) {
+        triangle.push([1])
+        return triangle
+    } else {
+        // for now incomplete
+    }
+};
+
+
+console.log(generate(1))
