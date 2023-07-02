ac3b5391de31eeb90b82d23e61cb23b898304801
Tidalu <jsjsdq65@gmail.com>
2023-07-03 00:20:22 +0200

    done, moves zero

--- _Logic_Building_/moveZeros.js
+++ _Logic_Building_/moveZeros.js
@@ -1,10 +1,25 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead. -- okay
- */
+
 var moveZeroes = function (nums) {
+    var nonZeroIndex = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[nonZeroIndex] = nums[i];
+            nonZeroIndex++;
+        }
 
-};
+        // while (nums[i] !== 0) {
+        //     nums[nonZeroIndex] = nums[i]
+        //     nonZeroIndex++
+        // } // sucks for a moment, i should've known, while loops stop iterating whenever it came to 0, as soon as it checks for value 0 then it will go with the rest of the for loop, but if else checks for all elements
 
+    }
 
-console.log([4, 3, 0, 2, 6, 0, 7, 0, 0])
+    for (let i = nonZeroIndex; i < nums.length; i++) {
+        nums[i] = 0
+    }
+    return nums
+}
+
+console.log(moveZeroes([4, 3, 0, 2, 6, 0, 7, 0, 0]))
+console.log(moveZeroes([0]))
+console.log(moveZeroes([0, 1, 0, 3, 12]))
