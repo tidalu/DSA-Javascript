3f62550795a85a41df34e1fec762b259d7dbed88
Tidalu <jsjsdq65@gmail.com>
2023-07-08 16:38:13 +0200

    dene:best time to buy and sell

--- _Logic_Building_/bestTimeBuySellStock.js
+++ _Logic_Building_/bestTimeBuySellStock.js
@@ -2,30 +2,76 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    var decreasing = true;
-    var buy = 0;
-    for(var i = 0; i < prices.length-1; i++){
-        if(prices[i] > prices[i+1]){
-            decreasing = false
-            buy = prices[i+1]
-            break;
+var maxProfit_ = function (prices) {
+    if (prices.length == 0) {
+        return 0
+    }
+    // var max = Math.max(...prices);
+    var min = Math.min(...prices)
+
+    // console.log('Min', min)
+    // var indexMax = prices.indexOf(max);
+    var indexMin = prices.indexOf(min) + 1;
+    // console.log('IndexMin', indexMin)
+    // if(indexMin<indexMax){
+    //     console.log('condition is true')
+    //     return max - min;
+    // }
+    var newArr = prices.slice(indexMin);
+    // console.log(newArr)
+
+    if (newArr.length !== 0) {
+        var max = Math.max(...newArr);
+        var profit = max - min;
+        return profit;
+    } else if (newArr.length == 0) {
+        prices.pop();
+        return maxProfit(prices);
+    }
+
+
+    // if (prices.length > 1) {
+    //     newArr.pop();
+    //     return maxProfit(newArr);
+    // } else {
+    //     return 0;
+    // }
+};
+//
+
+// second version
+
+var maxProfit_ = function (prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
         }
     }
-    if(decreasing){
-        return 0;
-    }else {
-        return buy;
-        // for(var i = 0; i < prices.length-1; i++){
-        //     if(prices[i] > prices[i+1]){
-        //         decreasing = false
-        //     }
-        // }
-    }
+    return maxProfit;
 };
 
-// unfinished
 
-console.log(maxProfit([7,1,5,3,6,4]))
-console.log(maxProfit([7,6,4,3,1]))
-console.log(maxProfit([2, 4, 1]))
+
+
+// third vay 
+
+function maxProfit(prices) {
+    var  min = prices[0];
+    var  max = 0
+
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i])
+        max = Math.max(max, prices[i] - min)
+    }
+
+    return max
+};
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))//5
+console.log(maxProfit([7, 6, 4, 3, 1]))//0
+console.log(maxProfit([2, 4, 1]))//2
+console.log(maxProfit([2, 1, 2, 1, 0, 1, 2]))// 2
+console.log(maxProfit([3, 2, 6, 5, 0, 3])) // 4
