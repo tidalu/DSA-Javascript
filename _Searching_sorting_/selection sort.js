3a3dd29a8786068595c967d32ad6ac12ff027b67
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-26 14:46:55 +0100

    update : time complexity analysis

--- _Searching_sorting_/selection sort.js
+++ _Searching_sorting_/selection sort.js
@@ -53,8 +53,8 @@ console.log(selectionLoop([1, 4, 2, 8,-10,  12, 17, 2, 3, 56, 1]))
 
 // we run two nested loops where the comparison, swapping, and updating of the minIndex are key operations, regardless of the input , comparison and swapping operations will execute everytime , but updating minIndex depends on the order of input values 
 
-// at the ith  iteration of the puter loop, the inner loop will run n-i times, so the total count of nested loop iterations = 
-// Sum of (n - i) from i = 0 to n - 1 = n+ (n-1) + (n-2)+...+2+1  = n(n+1)/2  = O(n^2)
+//O(N2), (where N = size of the array), for the best, worst, and average cases.
+// Reason: If we carefully observe, we can notice that the outer loop, say i, is running from 0 to n-2 i.e. n-1 times, and for each i, the inner loop j runs from i to n-1. For, i = 0, the inner loop runs n-1 times, for i = 1, the inner loop runs n-2 times, and so on. So, the total steps will be approximately the following: (n-1) + (n-2) + (n-3) + ……..+ 3 + 2 + 1. The summation is approximately the sum of the first n natural numbers i.e. (n*(n+1))/2. The precise time complexity will be O(n2/2 + n/2)
 
 
 /**
