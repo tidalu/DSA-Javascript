0efdd42039f5cd8f4a4002d852ae15c05daae2de
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-05 20:07:07 +0100

    second way of solving : move zeros

--- _Logic_Building_/moveZeros.js
+++ _Logic_Building_/moveZeros.js
@@ -23,3 +23,32 @@ var moveZeroes = function (nums) {
 console.log(moveZeroes([4, 3, 0, 2, 6, 0, 7, 0, 0]))
 console.log(moveZeroes([0]))
 console.log(moveZeroes([0, 1, 0, 3, 12]))
+
+// second approach 
+
+var moveZeroes = function(nums) {
+    var count = 0;
+    var i = 0;
+
+    while (i < nums.length) {
+        if (nums[i] == 0) {
+            nums.splice(i, 1);
+            count++;
+        } else {
+            i++;
+        }
+    }
+
+    while (count > 0) {
+        nums.push(0);
+        count--;
+    }
+
+    return nums;
+};
+
+
+// Time Complexity: O(N), N = size of the array.
+// Reason: We have used 2 loops and using those loops, we are basically traversing the array once.
+
+// Space Complexity: O(1) as we are not using any extra space to solve this problem.
