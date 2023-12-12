fdcc8e7eb219cfa8ecc4c89b36f17f2940e6670d
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2023-12-12 23:36:46 +0100

    update: reformat

--- _Arrays_/majorityElement.js
+++ _Arrays_/majorityElement.js
@@ -1,25 +1,24 @@
+var majorityElement = function (nums) {
+  let count = 0;
+  let el;
 
-var majorityElement = function(nums) {
-    let count = 0; 
-  let el ;
-  
-  for(let i = 0; i< nums.length; i++) {
-      if(count == 0) {
-        count = 1
-        el = nums[i]
-      } else if(nums[i] == el) {
-        count++
-      } else {
-        count--
-      }
+  for (let i = 0; i < nums.length; i++) {
+    if (count == 0) {
+      count = 1;
+      el = nums[i];
+    } else if (nums[i] == el) {
+      count++;
+    } else {
+      count--;
+    }
   }
   let c = 0;
-  for(let j = 0; j < nums.length; j++) {
-    if(nums[j] == el ) c++
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] == el) c++;
   }
-  
-  if(c > (nums.length / 2)) {
-    return el
+
+  if (c > nums.length / 2) {
+    return el;
   }
 };
 
