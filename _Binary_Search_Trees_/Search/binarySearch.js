67b6c5068c7b1c46ca6e5af26737f2de9d50eb5c
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2024-01-01 16:58:11 +0100

    somoe edge cases covered

--- _Binary_Search_Trees_/Search/binarySearch.js
+++ _Binary_Search_Trees_/Search/binarySearch.js
@@ -26,10 +26,17 @@ var search = function (nums, target) {
   return -1;
 };
 
-// time complexity is O(logn)
+// time complexity is O(log2n) - log base 2 n
 
 //// intuition
 // there what we are doing is , we are geassing that the target is there, but more efficient guess,
 // we are having a var mid, and the value is the middle index of the nums , then we are chekcing
 // if target is whihc side, if it is greater that target then we should look for only the left subarray , else right subarray, /
 // whenever the mid index value if the nums is equal to target , then we just return the mid index, else retuurn -1
+
+// theory
+
+// binary search is applicable only when the [data is sorted]
+// overflow case, to avoid this , if we are sure that the arget is between first and last, we can assign mid as this:
+// mid = first + (last - first) / 2
+// in this case when data is so high, last is MAX_SAFE_INTEGER, in our normal condition it gets like (MAX_SAFE_INTEGER + MAX_SAFE_INTEGER)/2 which gets overflow, so that we can use the mid assigning above which is safer,
