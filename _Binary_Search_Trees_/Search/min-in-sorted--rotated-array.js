2a812048b499717f6778b700f1914e733066cd89
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2024-01-04 22:36:08 +0100

    solved: min in sorted rotated array

---
+++ _Binary_Search_Trees_/Search/min-in-sorted--rotated-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let num = nums.length;
+  let low = 0;
+  let high = num - 1;
+  let ans = nums[0];
+
+  while (low <= high) {
+    let mid = Math.floor(low + (high - low) / 2);
+
+    if (nums[low] <= nums[mid]) {
+      ans = Math.min(ans, nums[low]);
+      low = mid + 1;
+    } else {
+      ans = Math.min(ans, nums[mid]);
+
+      high = mid - 1;
+    }
+  }
+
+  return ans;
+};
