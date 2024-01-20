b70661982be9c29119e65486e762cd1db34dc62c
Ulugbek Norbutaev <ul0271069@edu.uni.lodz.pl>
2024-01-20 20:11:55 +0100

    deleting in linket list

--- _Linked_List_/basics.js
+++ _Linked_List_/basics.js
@@ -54,4 +54,70 @@ function check(head, value) {
   return { idx: -1, status: false };
 }
 
-console.log(check(resultNode, 3));
+// console.log(check(resultNode, 3));
+
+function deleteHead(head) {
+  if (head == null) return head;
+
+  head = head.next;
+
+  return head; // we should not care about the first head in teh memory, cuz in js there is garbage collector taht will automatically delete it by itself, in the future
+}
+
+function printLL(head) {
+  let temp = head;
+  while (temp) {
+    console.log(temp.data);
+    temp = temp.next;
+  }
+}
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let result = convertLA(arr1);
+result = deleteHead(result);
+
+// delete the tail
+
+function removeTail(head) {
+  if (head == null || head.next == null) return null;
+  let temp = head;
+  while (temp.next.next) {
+    temp = temp.next;
+  }
+
+  temp.next = null;
+  return head;
+}
+
+result = removeTail(result);
+
+// remove kth element in ll
+
+function removeK(head, k) {
+  if (head == null) return head;
+
+  if (k == 1) {
+    head = head.next;
+    return head;
+  }
+
+  let perv = null;
+  let count = 0;
+  let temp = head;
+  while (temp) {
+    count++;
+    if (count == k) {
+      perv.next = perv.next.next;
+
+      return head;
+    }
+
+    perv = temp;
+    temp = temp.next;
+  }
+
+  return head;
+}
+
+result = removeK(result, 3);
+printLL(result);
